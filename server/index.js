bf3dd6dbbcf86abdd6eff99f88e2ef02283cf5fe
N ASIFKHAN <asifkhan4092@gmail.com>
2024-12-25 20:09:40 +0530

    server changed

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app=express();
 app.use(express.json())
 app.use(
   cors({
-      origin: ["http://localhost:5173","https://hospital-client-phi.vercel.app"] ,
+      origin: ["http://localhost:5173","https://hospital-management-client-one.vercel.app"] ,
       credentials: true,
       methods: ["GET","POST","PUT","DELETE"]
   })
